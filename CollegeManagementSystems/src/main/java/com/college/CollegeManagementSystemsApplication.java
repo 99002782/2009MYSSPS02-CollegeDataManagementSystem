649859e49cbc50b56eaa6afbd0371b4ef492ee4f
99002782 <72496568+99002782@users.noreply.github.com>
2020-11-21 13:31:46 +0530

    Add files via upload

---
+++ CollegeManagementSystems/src/main/java/com/college/CollegeManagementSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.college;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeManagementSystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeManagementSystemsApplication.class, args);
+	}
+
+}
---
+++ CollegeManagementSystems/src/main/java/com/college/controllers/CollegeController.java
@@ -0,0 +1,5 @@
+package com.college.controllers;
+
+public class CollegeController {
+
+}
